9954ae8e0c4252813e2564d3a23c7ebd25073e45
ray <raymondzone1495@gmail.com>
2020-12-02 14:29:12 +0800

    2/12/20 1429

--- proj/src/Admin.java
+++ proj/src/Admin.java
@@ -1,3 +1,26 @@
+package admin;
+
+import java.util.Scanner
+
 public class Admin {
-    
+    public static void main(String[] args){
+        Scanner myObj
+        System.out.println("Administrator, what would you like to do?");
+        System.out.println("1. Create tables");
+        System.out.println("2. Delete tables");
+        System.out.println("3. Load data");
+        System.out.println("4. Check data");
+        System.out.println("5. Go back");
+        System.out.println("Please enter [1-5]");
+
+
+    }
+
+    public static void load_data(){
+
+    }
+
+    public static void check_data(){
+
+    }
 }
